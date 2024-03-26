9eab87672786d4d1629ccc74f666182415d63659
Yutabhai <155735358+Yutabhai@users.noreply.github.com>
2024-03-26 21:07:57 +0530

    Update __init__.py
    Signed-off-by: Yutabhai <155735358+Yutabhai@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -14,8 +14,8 @@ LOG.setLevel(level=logging.INFO)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
 
-API_ID = os.environ.get("23902408") #API ID
-API_HASH = environ.get('API_HASH', '6a36a4ef2f07d63aeba7b53b99c64d73') #API HASH
+API_ID = os.environ.get("28640015") #API ID
+API_HASH = environ.get('API_HASH', 'e8f539f9fcca3eb5284edababf5062fe') #API HASH
 BOT_TOKEN = environ.get('BOT_TOKEN', '7190718780:AAEoMKzvj9Mb16iipToTw8uuwCrhkth0yYY') #BOT TOKEN
 DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://pietar19sc:A0jqNJO96p3MQfOg@cluster0.467yujo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') #MONGO DB
 OWNER_ID = os.environ.get("7179837246") #OWNER ID
