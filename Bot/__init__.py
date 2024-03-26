2e8ce2214a47aace4b8cf7d7b326e175dc292694
Yutabhai <155735358+Yutabhai@users.noreply.github.com>
2024-03-26 20:29:32 +0530

    Update __init__.py
    Signed-off-by: Yutabhai <155735358+Yutabhai@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -17,9 +17,9 @@ API_ID = os.environ.get("23902408") #API ID
 API_HASH = environ.get('API_HASH', '6a36a4ef2f07d63aeba7b53b99c64d73') #API HASH
 BOT_TOKEN = environ.get('BOT_TOKEN', '7190718780:AAEoMKzvj9Mb16iipToTw8uuwCrhkth0yYY') #BOT TOKEN
 DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://pietar19sc:A0jqNJO96p3MQfOg@cluster0.467yujo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') #MONGO DB
-OWNER_ID = int(environ.get("7179837246")) #OWNER ID
-MAIN_CHANNEL = int(environ.get("-1001951273334"))#YOUR MAIN CHANNEL ID
-ARCHIVE_CHANNEL = int(environ.get("-1001692585152"))#YOUR ARCHIVE CHANNEL
-MESSAGE_ID = int(environ.get("-1002059315034")) #SUB CHANNEL STATUS ID
+OWNER_ID = os.environ.get("7179837246") #OWNER ID
+MAIN_CHANNEL = os.environ.get("-1001951273334") #YOUR MAIN CHANNEL ID
+ARCHIVE_CHANNEL = os.environ.get("-1001692585152") #YOUR ARCHIVE CHANNEL
+MESSAGE_ID = os.environ.get("-1002059315034") #SUB CHANNEL STATUS ID
 
 soheru = Client('SoheruBots', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
